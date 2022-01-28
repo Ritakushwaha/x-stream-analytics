a167571c76dbcd6dcf23f330148b3bc84d18ed95
Ritakushwaha <ritakushwaha731997@gmail.com>
2022-01-29 00:22:09 +0530

    Twitter data streaming with pyspark kafka and first 5 trending hashtags relaed to Omicron displayed using matplotlib

--- twitter_data_with_kafka_pyspark/kafka-pyspark-twitter-streaming.py
+++ twitter_data_with_kafka_pyspark/kafka-pyspark-twitter-streaming.py
@@ -6,20 +6,21 @@ Created on Tue Jan 11 16:53:46 2022
 @author: rita
 """
 
-# python kafka_prod_demo.py
+# python Kafka_producer_pyspark.py
 
 from pyspark.sql.types import StructType, StringType, StructField, DateType, ArrayType, MapType
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, split, window, substring
 from pyspark.sql.functions import col, from_json
-#from bson import BSON
-#from bson import json_util
-import matplotlib.pyplot as plt
+import time
+import pandas as pd
+import os
+from collections import Counter
+import re
+import matplotlib.pyplot as plt, mpld3
 
 kafka_topic_name = "trump"
-
 kafka_bootstrap_servers = "localhost:9092"
-
 nestTimestampFormat = "yyyy-MM-dd'T'HH:mm:ss.sss'Z'"
 jsonOptions = {"timestampFormat": nestTimestampFormat}
 
@@ -32,6 +33,38 @@ def all_data(posts_df):
     .start()
                     
     col_stream.awaitTermination(1)
+    
+def remove_old_file(path):
+    for filename in os.listdir(path):
+        if os.path.isfile(os.path.join(path, filename)):
+            os.remove(os.path.join(path, filename))
+            
+def reading_and_plotting(path):       
+    files = os.listdir(path)
+    result =[]
+    
+    for file in files:
+        if file.endswith(".csv"):
+            df = pd.read_csv(file,names=["Location","Hashtags","Time"])
+            for hashtag in df["Hashtags"]:
+                words = re.findall('[a-zA-Z0-9]+', hashtag)
+                for word in words:
+                    result.append(word.lower())
+    
+    c = Counter(result)
+    count_dict=dict(c)
+    count_dict_descending = dict(sorted(count_dict.items(), key = lambda kv:(kv[1], kv[0]),reverse=True))
+    first5pairs = {k: count_dict_descending[k] for k in list(count_dict_descending)[:5]}
+    if len(first5pairs)>0:
+        word_count = first5pairs.items()
+        x, y = zip(*word_count)
+        plt.plot(x, y,color='red', marker='o')
+        plt.plot(x, y)
+        plt.xlabel('Hashtags')
+        plt.ylabel('Counts')
+        plt.title('Trending Hashtags Counts')
+        plt.show()
+        #mpld3.show()
 
 
 def hashtags_globally(posts_df):
@@ -43,46 +76,30 @@ def hashtags_globally(posts_df):
             .filter(col("Hashtags").contains('Omicron' or 'omicron')) \
             .filter(col("User_location") != "")
 
-        '''
-        hashtags_write_location = hashtags_location.writeStream.trigger(processingTime="120 seconds")\
+        hashtags_write_location = hashtags_location.writeStream.trigger(processingTime="60 seconds")\
             .outputMode('append')\
             .option("truncate", "false")\
-            .option("checkpointLocation", ".checkpoint/h")\
+            .option("checkpointLocation", "check")\
             .format("csv")\
-            .option("path","./excel.csv")\
+            .option("path","./data")\
             .start()
+        
+        os.chdir(os.getcwd()+"/data")
+        path = os.getcwd()
 
-        hashtags_write_location.awaitTermination()'''
-
-        hashtags_count_per_location = hashtags_location.groupBy("timestamp","Hashtags").count().select('*')
-
-        hashtags_count_location = hashtags_count_per_location.writeStream.trigger(processingTime='60 seconds')\
-            .outputMode('update')\
-            .option("truncate", "false")\
-            .option("checkpointLocation", "./checkpoint/n")\
-            .format("console")\
-            .option("failOnDataLoss", "false")\
-            .start()
-
-        hashtags_count_location.awaitTermination()
+        def periodic_work(interval):
+            while True:
+                reading_and_plotting(path)
+                remove_old_file(path)
+                hashtags_write_location.awaitTermination(1)
+                # interval should be an integer, the number of seconds to wait
+                time.sleep(interval)
+                
+        periodic_work(60*0.1)
+            
+        
     except Exception as e:
         print(e)
-
-
-def hashtags_india(hashtags_location):
-    '''==============================================Hashtags, Location - India============================================'''
-    hashtags_india = hashtags_location \
-        .select('Hashtags', 'User_location') \
-        .filter(col("User_location").like("%India%"))
-
-    hashtags_india_data = hashtags_india.writeStream.trigger(processingTime='5 seconds')\
-        .outputMode('update')\
-        .option("truncate", "false")\
-        .option("checkpointLocation", "./tree")\
-        .format("console")\
-        .start()
-
-    hashtags_india_data.awaitTermination()
 
 
 if __name__ == "__main__":
@@ -115,7 +132,7 @@ if __name__ == "__main__":
     posts_df.printSchema()
 
 
-    # all data
+    #all data
 
     #all_data(posts_df)
 
